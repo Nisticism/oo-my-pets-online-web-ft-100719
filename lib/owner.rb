386883b5a8b44d77c3c640ced8a39df3b1be1e8f
Hans Foster <fosterhans@gmail.com>
2019-11-08 22:13:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+    
+    attr_reader :name, :species
+    
+  def initialize(name = "", species = "human")
+    @name = name
+    @species = species
+  end
+  
+  def say_species
+    puts "#{@species}"
+  end
+  
+  
 end
