7ca78c945c92469c1207847c5681d027573ecd26
Hans Foster <fosterhans@gmail.com>
2019-11-08 22:28:17 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,7 +5,7 @@ class Cat
   
   @@all = []
   
-  def initialize(name, owner, mood = "nervous")
+  def initialize(name, owner = "", mood = "nervous")
     @name = name 
     @owner = owner
     @mood = mood
